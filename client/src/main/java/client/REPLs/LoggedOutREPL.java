b1ea97e6b569618e06640b0495f529e63d2799b7
Xander Bystrom <bystromxander@gmail.com>
2025-11-04 11:06:32 -0700

    Implemented 'help' command for LoggedOutREPL

--- client/src/main/java/client/REPLs/LoggedOutREPL.java
+++ client/src/main/java/client/REPLs/LoggedOutREPL.java
@@ -2,6 +2,9 @@ package client.REPLs;
 
 import java.util.Scanner;
 
+import static ui.EscapeSequences.SET_TEXT_COLOR_MAGENTA;
+import static ui.EscapeSequences.SET_TEXT_COLOR_WHITE;
+
 public class LoggedOutREPL extends REPL {
     public LoggedOutREPL(Scanner scanner) {
         this.scanner = scanner;
@@ -35,6 +38,18 @@ public class LoggedOutREPL extends REPL {
     }
 
     private String help() {
+        System.out.print(SET_TEXT_COLOR_MAGENTA + "login <USERNAME> <PASSWORD> - ");
+        System.out.println(SET_TEXT_COLOR_WHITE + " to access Chess functionality");
+
+        System.out.print(SET_TEXT_COLOR_MAGENTA + "register <USERNAME> <PASSWORD> <EMAIL> - ");
+        System.out.println(SET_TEXT_COLOR_WHITE + " to create an account");
+
+        System.out.print(SET_TEXT_COLOR_MAGENTA + "help - ");
+        System.out.println(SET_TEXT_COLOR_WHITE + " to display available commands");
+
+        System.out.print(SET_TEXT_COLOR_MAGENTA + "quit - ");
+        System.out.println(SET_TEXT_COLOR_WHITE + " to exit the program");
+
         return "help";
     }
 }
